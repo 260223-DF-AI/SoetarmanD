e67afd2c8fd8c6920a4fc604f5b1dab6eb88d882
Dio Soetarman <acsoetarman@gmail.com>
2026-03-06 15:30:03 -0600

    Added very simple tests for timer and retry decorators

--- Exercises/W2/DCPractice/tests/test_decorators.py
+++ Exercises/W2/DCPractice/tests/test_decorators.py
@@ -3,11 +3,39 @@ from src.decorators import timer, retry, cache
 
 def test_timer_returns_result():
     """Timer decorator should not affect return value."""
-    pass
+    # ARRANGE
+    actual = 0
+    expected = 5 + 5
+    @timer
+    def func(a, b):
+        return a + b
+    
+    # ACT
+    actual = func(5, 5)
+    # ASSERT
+    assert actual == expected
 
 def test_retry_succeeds_eventually():
     """Retry should succeed if function works within attempts."""
-    pass
+    # ARRANGE
+    actual = 0
+    expected = 10
+    expected2 = None
+    
+    @retry(1, 0.5)
+    def func1(a, b):
+        return a + b
+    
+    @retry(0, 0.5)
+    def func2(a, b):
+        return a + b
+    # ACT
+    actual = func1(5, 5)
+    actual2 = func2(5, 5)
+
+    # ASSERT
+    assert actual == expected
+    assert actual2 == expected2
 
 def test_cache_returns_cached_value():
     """Cache should return same value without recomputing."""
